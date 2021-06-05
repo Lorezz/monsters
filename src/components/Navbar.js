cd206a7a0ef37223daee469b62ff9fc01a3d4970
Lorezz <point.point@gmail.com>
2021-06-05 02:57:23 +0200

    wip dialogs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,8 @@ import {
   MenuItem,
   MenuDivider,
   // useBreakpointValue,
+  Kbd,
+  Text,
   useDisclosure,
   Heading,
   Button,
@@ -17,18 +19,120 @@ import {
   ModalHeader,
   ModalBody,
   ModalCloseButton,
+  ListItem,
+  UnorderedList,
 } from '@chakra-ui/react';
 import { MdSave, MdMoreVert, MdFileUpload, MdShare } from 'react-icons/md';
 import { FaGithub } from 'react-icons/fa';
 // import { Helmet } from 'react-helmet';
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+} from 'react-share';
 
 import ImportSection from './ImportSection';
 import { FabricContext } from '../lib/ctx';
 import * as api from '../lib/api';
 import { openUrl } from '../lib/utils';
 
-const About = () => <div>ciao</div>;
-const Keys = () => <div>keys</div>;
+const shareUrl = 'https://monsters-factory.netlify.app';
+
+const About = () => (
+  <Box p={10}>
+    <Heading color="gray.100">Hi,</Heading>
+    <Text fontSize="lg" pt={5}>
+      This is a place where you can play with svg. You can use the body parts
+      inside this page, or you can also import external svg images.
+    </Text>
+    <Text fontSize="lg" pt={5}>
+      {` The template of monster's parts in the site is priveded by `}
+      <a href="https://it.freepik.com/home" target="_blank">
+        freepik.com
+      </a>
+      .
+    </Text>
+    <Text fontSize="lg" pt={5}>
+      {`This is an open-source project, you can find the code on `}
+      <a href="https://github.com/Lorezz/monsters" target="_blank">
+        github.com
+      </a>
+      .
+    </Text>
+    <Text fontSize="lg" pt={5}>
+      Hope you like and have some fun.
+    </Text>
+    <Heading pt={5} color="teal.200">
+      Lorezz.
+    </Heading>
+  </Box>
+);
+const Keys = () => (
+  <Box mb={10}>
+    <UnorderedList spacing={2}>
+      <ListItem>
+        <Kbd>Ctrl z</Kbd> Undo
+      </ListItem>
+      <ListItem>
+        <Kbd>Ctrl y</Kbd> Redo
+      </ListItem>
+      <ListItem>
+        <Kbd>Ctrl g</Kbd>Group
+      </ListItem>
+      <ListItem>
+        <Kbd>Ctrl u</Kbd> Ungroup
+      </ListItem>
+      <ListItem>
+        <Kbd>Ctrl a</Kbd> Select All
+      </ListItem>
+      <ListItem>
+        <Kbd>Esc</Kbd> Deselect All
+      </ListItem>
+      <ListItem>
+        <Kbd>Ctrl +</Kbd> Zoom In
+      </ListItem>
+      <ListItem>
+        <Kbd>Ctrl -</Kbd> Zoom Out
+      </ListItem>
+      <ListItem>
+        <Kbd>Ctrl v</Kbd> Duplicate
+      </ListItem>
+      <ListItem>
+        <Kbd>Backspace</Kbd> Delete
+      </ListItem>
+      <ListItem>
+        <Kbd>t</Kbd> Add Text
+      </ListItem>
+      <ListItem>
+        <Kbd>CMD/WIN LeftArrow</Kbd> Send To Back
+      </ListItem>
+      <ListItem>
+        <Kbd>LeftArrow</Kbd> Send Backwards
+      </ListItem>
+      <ListItem>
+        <Kbd>CMD/WIN RightArrow</Kbd> Bring To Front
+      </ListItem>
+      <ListItem>
+        <Kbd>RightArrow</Kbd> Bring Forward
+      </ListItem>
+    </UnorderedList>
+  </Box>
+);
+const Share = () => (
+  <Flex alignItems="center" justifyContent="space-around" my={10}>
+    <TwitterShareButton url={shareUrl}>
+      <Button colorScheme="twitter">Twitter</Button>
+    </TwitterShareButton>
+
+    <FacebookShareButton url={shareUrl}>
+      <Button colorScheme="facebook">Facebook</Button>
+    </FacebookShareButton>
+
+    <WhatsappShareButton url={shareUrl}>
+      <Button colorScheme="teal">Whatsapp</Button>
+    </WhatsappShareButton>
+  </Flex>
+);
 
 const Nav = () => {
   // const isSM = useBreakpointValue({ base: true, md: false });
@@ -49,6 +153,8 @@ const Nav = () => {
       info = { title: 'Shortcuts', type };
     } else if (type === 'import') {
       info = { title: 'Upload SVG or JSON', type };
+    } else if (type === 'share') {
+      info = { title: 'Share this site adding your creation', type };
     }
     setModalInfo(info);
     onOpen();
@@ -146,7 +252,10 @@ const Nav = () => {
               >
                 {`Star/Fork this project`}
               </MenuItem>
-              <MenuItem icon={<MdShare size={24} />}>
+              <MenuItem
+                icon={<MdShare size={24} />}
+                onClick={() => handleModal('share')}
+              >
                 Share your Monster
               </MenuItem>
             </MenuList>
@@ -162,6 +271,8 @@ const Nav = () => {
                   <ImportSection />
                 ) : modalInfo?.type === 'keys' ? (
                   <Keys />
+                ) : modalInfo?.type === 'share' ? (
+                  <Share />
                 ) : (
                   <About />
                 )}

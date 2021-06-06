bd273f607a2935ec8dc42f6336a0523dad8e0d8d
Lorezz <point.point@gmail.com>
2021-06-06 15:34:09 +0200

    removed share moster

--- src/components/Share.js
+++ src/components/Share.js
@@ -1,46 +1,39 @@
-import { Flex, Button, Box } from '@chakra-ui/react';
-import saveAs from 'save-as';
-
+import { Flex, Button } from '@chakra-ui/react';
 import {
   FacebookShareButton,
   TwitterShareButton,
   WhatsappShareButton,
 } from 'react-share';
-import { useContext } from 'react';
-// import { MdSave, MdMoreVert, MdFileUpload, MdShare } from 'react-icons/md';
-import { Helmet } from 'react-helmet';
-
-import { FabricContext } from '../lib/ctx';
-import { svgToB64 } from '../lib/utils';
+// import { useContext } from 'react';
+// import { Helmet } from 'react-helmet';
+// import { FabricContext } from '../lib/ctx';
+// import { svgToB64 } from '../lib/utils';
 const Share = () => {
-  const [canvas] = useContext(FabricContext);
   const shareUrl = 'https://monsters-factory.netlify.app';
-
-  if (!canvas) {
-    return null;
-  }
-  const w = canvas?.getWidth();
-  const h = canvas?.getHeight();
-  const image = canvas.toDataURL({
-    width: canvas.width,
-    height: canvas.height,
-    left: 0,
-    top: 0,
-    format: 'png',
-  });
+  // const [canvas] = useContext(FabricContext);
+  // if (!canvas) {
+  //   return null;
+  // }
+  // const w = canvas?.getWidth();
+  // const h = canvas?.getHeight();
+  // const image = canvas.toDataURL({
+  //   width: canvas.width,
+  //   height: canvas.height,
+  //   left: 0,
+  //   top: 0,
+  //   format: 'png',
+  // });
 
   return (
     <Flex alignItems="center" justifyContent="space-around" my={10}>
-      <Helmet>
+      {/* <Helmet>
         <meta property="og:image" content={image} />
         <meta property="og:image:width" content={w} />
         <meta property="og:image:height" content={h} />
         <meta name="twitter:image" content={image} />
-      </Helmet>
-
-      <Box>
+      </Helmet>   <Box>
         <img src={image} lazy="true" width={100} />
-      </Box>
+      </Box> */}
       <TwitterShareButton url={shareUrl}>
         <Button colorScheme="twitter">Twitter</Button>
       </TwitterShareButton>
